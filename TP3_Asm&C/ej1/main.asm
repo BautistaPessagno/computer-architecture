e9fb4851b98565249532e1ed91d361a79f91fdcf
Pessagno Bautista <bpessagno@itba.edu.ar>
2025-12-12 11:12:09 -0300

    tp3/ej1

--- TP3_Asm&C/ej1/main.asm
+++ TP3_Asm&C/ej1/main.asm
@@ -3,30 +3,36 @@ GLOBAL main
 EXTERN printf
 
 section .rodata
-  fmt db "Cantidad de argumentos: %d\n", 10
-  fmt2 db "Argumento %d: %d", 10
+  fmt db "Cantidad de argumentos: %d", 10, 0
+  fmt2 db "Argumento %d: %s", 10, 0
 
 section .text
 
   main:
       push ebp ;Armado de stack frame
       mov ebp, esp ;
-      push dword [ebp+8]
+
+      ;consigo las variables
+      mov edi, dword [ebp+8]
+      mov ebx, [ebp+12]
+
+      push edi
       push fmt
       call printf
       add esp, 2*4
       
-      ;consigo la cantidad de argumentos
-      mov eax, [ebp+8]
-      mov esi, 0
+      xor esi, esi ; esi = 0
       .loop:
-          cmp esi, eax
+          cmp esi, edi
           je .end
-          inc esi
-          push dword [ebp+8+esi*4]
+          mov ecx, [ebx+esi*4]
+          push ecx
           push esi
           push fmt2
           call printf
+
+
+          inc esi
           add esp, 3*4
           jmp .loop
         
